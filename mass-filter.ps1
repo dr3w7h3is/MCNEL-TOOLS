6fa265bc85176ac49b91c1c7cec5074a0d2a082a
Dr3w7h3is <drew.theis.1@gmail.com>
2019-09-05 11:48:22 -0700

    update

--- mass-filter.ps1
+++ mass-filter.ps1
@@ -1,11 +1,11 @@
 # Author: Drew Theis
-# Date: 2/16/19
+# Date: 9/5/2019
 # Description: Allows filter to be applied to many .pcap/pcapng at once to speed up processing ran form terminal
 # Version: 0.1.1
 # Updates: Added support to check if file already exsits in destination directory to skip processing, prevent data from being over written and allows
-# for the processing to be picked up where it left off in the event of an outage.
-$sourceFolder = "E:\MCICOM 2019-8-22\MCFMIS\Server\0822_MCFIMIS_Server_BLDG33302_TR1\DP-B To MLX16"
-$destinationFolder = "E:\MCICOM 2019-8-22\MCFMIS\Server\FilterToOnlyServer\Test Run 1 - 33 Area Chow hall\TAP B"
+#          for the processing to be picked up where it left off in the event of an outage.
+$sourceFolder = "C:\Some\File\Location"
+$destinationFolder = "C:\Some\New\File\Location"
 $cap = Get-ChildItem $folder
 foreach ($singleCap in $cap) {
     If ((Test-Path $destinationFolder\$singleCap) -notcontains $true ) {

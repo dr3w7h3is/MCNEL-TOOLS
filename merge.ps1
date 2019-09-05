dfc457cfa948affe5f8ce1d2e4c8ec98f96b329c
Dr3w7h3is <drew.theis.1@gmail.com>
2019-09-05 13:37:56 -0700

    Update to prompt for file name

--- merge.ps1
+++ merge.ps1
@@ -1,9 +1,10 @@
 # Author: Drew Theis
 # Date: 9/5/2019
 # Description: Terminal base script to allow merger of many files
-# Version: 0.0.2
-# Update: Fixed bug issue that would identify file as not exsisting if directory wasn't changed to source path
+# Version: 0.0.3
+# Update: Allow user to name merged PCAP
 $folder = "C:\Some\File\Location"
+$name = Read-Host -Prompt "Input file name: "
 $files = (Get-ChildItem -Path $folder)
 Set-Location -Path $folder
-C:\'Program Files'\Wireshark\mergecap.exe -w merge.pcap $files 
+C:\'Program Files'\Wireshark\mergecap.exe -w $name $files 
